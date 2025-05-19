27bb6dbbd97eb3013ba2a7af369a360cbbd353f0
Kazem <kazemjavadi@hotmail.com>
2025-05-19 12:15:59 +0330

    Redis sample code for STRING commands

---
+++ 007.Redis.DataStructure.STRING.Command/Program.cs
@@ -0,0 +1,29 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+var db = redis.GetDatabase();
+const string key_integer_value = "key_integer_value";
+db.StringSet(key_integer_value, 10);
+
+//INC, DECR, INCBY, DECBY
+
+Console.WriteLine(db.StringGet(key_integer_value));
+db.StringIncrement(key_integer_value);
+Console.WriteLine(db.StringGet(key_integer_value));
+db.StringDecrement(key_integer_value);
+Console.WriteLine(db.StringGet(key_integer_value));
+db.StringIncrement(key_integer_value, 10);
+Console.WriteLine(db.StringGet(key_integer_value));
+db.StringDecrement(key_integer_value, 10);
+Console.WriteLine(db.StringGet(key_integer_value));
+db.StringIncrement(key_integer_value, -5);
+Console.WriteLine(db.StringGet(key_integer_value));
+db.StringDecrement(key_integer_value, -10);
+Console.WriteLine(db.StringGet(key_integer_value));
+
+
+Console.ReadLine();
+
+
+
+
