30c7836dcc453ac0545afc9c08c0c3a6a5ec1ace
Kazem <kazemjavadi@hotmail.com>
2025-05-21 15:38:26 +0330

    Redis LIST and SET data structure commands

--- 008.Redis.DataStructure.LIST.Command/Program.cs
+++ 008.Redis.DataStructure.LIST.Command/Program.cs
@@ -3,4 +3,62 @@
 ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
 var db = redis.GetDatabase();
 
-const string key = "list:key_01";
+const string key1 = "list:key_01";
+const string key2 = "list:key_02";
+
+db.KeyDelete(key1);
+db.KeyDelete(key2);
+
+//RPUSH, LPUSH, RPOP, LPOP, LINDEX, LRANGE, LTRIM
+db.ListLeftPush(key1, new RedisValue[] { "value01", "value02", "value03" });
+ShowListKey01Items();
+
+db.ListLeftPush(key1, "value00");
+ShowListKey01Items();
+
+db.ListRightPush(key1, "value04");
+ShowListKey01Items();
+
+var value = db.ListRightPop(key1);
+Console.WriteLine(value);
+ShowListKey01Items();
+
+value = db.ListLeftPop(key1);
+Console.WriteLine(value);
+ShowListKey01Items();
+
+value = db.ListGetByIndex(key1, 3);
+Console.WriteLine(value.HasValue ? value : "null");
+
+value = db.ListGetByIndex(key1, 1);
+Console.WriteLine(value.HasValue ? value : "null");
+ShowListKey01Items();
+
+var values = db.ListRange(key1, 0, 1);
+Console.WriteLine(string.Join(", ", values));
+ShowListKey01Items();
+
+for (int i = 5; i < 10; i++)
+    db.ListRightPush(key1, $"value{i:d2}");
+ShowListKey01Items();
+
+db.ListTrim(key1, 0, 5);
+ShowListKey01Items();
+
+db.ListRightPopLeftPush(key1, key2);
+ShowListKey01Items();
+ShowListKey02Items();
+
+Console.ReadLine();
+
+
+
+void ShowListKey01Items() => ShowListItems(key1);
+void ShowListKey02Items() => ShowListItems(key2);
+
+void ShowListItems(string key)
+{
+    var values = db.ListRange(key, 0, -1);
+
+    Console.WriteLine(string.Join(", ", values));
+}
---
+++ 009.Redis.DataStructure.SET.Command/Program.cs
@@ -0,0 +1,81 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+var db = redis.GetDatabase();
+
+const string key01 = "set:key:01";
+const string key02 = "set:key:02";
+db.KeyDelete(key01);
+db.KeyDelete(key02);
+
+//SADD, SREM, SISMEMBER, SCARD, SMEMBERS, SRANDMEMBER, SPOP, SMOVE, SDIFF, SDIFFSTORE, SINTER, SINTERSTORE, SUNION, SUNIONSTORE
+
+long numberOfMemeberInSet = db.SetAdd(key01, ["value01", "value03", "value02", "value04", "value06", "value05", "value08", "value09", "value10", "value11", "value12"]);
+ShowNumberOfMembersInTheSet(numberOfMemeberInSet);
+ShowSetMembersKey01();
+
+bool isDeleted = db.SetRemove(key01, "VALUE");
+Console.WriteLine(isDeleted);
+
+isDeleted = db.SetRemove(key01, "value01");
+Console.WriteLine(isDeleted);
+ShowSetMembersKey01();
+
+var setLength = db.SetLength(key01);
+Console.WriteLine($"Set length is: {setLength}");
+
+var setMembers = db.SetMembers(key01);
+Console.WriteLine(string.Join(", ", setMembers));
+
+Console.WriteLine();
+
+for (int i = 0; i < 3; i++)
+{
+    setMembers = db.SetRandomMembers(key01, 2);
+    Console.WriteLine(string.Join(", ", setMembers));
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < 3; i++)
+{
+    setMembers = db.SetRandomMembers(key01, -2);
+    Console.WriteLine(string.Join(", ", setMembers));
+}
+
+Console.WriteLine();
+
+for (int i = 0; i < 3; i++)
+{
+    var setMemeber = db.SetPop(key01);
+    Console.WriteLine(setMemeber);
+}
+ShowSetMembersKey01();
+
+Console.WriteLine();
+
+db.SetMove(key01, key02, "value12");
+Console.WriteLine("Key01 set members: ");
+ShowSetMembersKey01();
+Console.WriteLine("Key02 set members: ");
+ShowSetMembersKey02();
+
+
+Console.WriteLine();
+
+db.
+
+
+Console.ReadLine();
+
+void ShowSetMembersKey01()=>ShowSetMembers(key01);
+void ShowSetMembersKey02() => ShowSetMembers(key02);
+
+
+void ShowSetMembers(string key)
+{
+    var values = db.SetMembers(key);
+    Console.WriteLine(string.Join(", ", values));
+}
+
+void ShowNumberOfMembersInTheSet(long numberOfMemeberInSet) => Console.WriteLine($"Number of items in the set: {numberOfMemeberInSet}");
