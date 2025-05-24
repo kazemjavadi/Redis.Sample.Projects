93ed140104db7098e54111a035f316c846c7dd25
Kazem <kazemjavadi@hotmail.com>
2025-05-24 16:22:54 +0330

    Redis SET and HASH data structures commands

--- 009.Redis.DataStructure.SET.Command/Program.cs
+++ 009.Redis.DataStructure.SET.Command/Program.cs
@@ -5,6 +5,9 @@ var db = redis.GetDatabase();
 
 const string key01 = "set:key:01";
 const string key02 = "set:key:02";
+const string key03 = "set:key:03";
+const string key04 = "set:key:04";
+
 db.KeyDelete(key01);
 db.KeyDelete(key02);
 
@@ -14,6 +17,7 @@ long numberOfMemeberInSet = db.SetAdd(key01, ["value01", "value03", "value02", "
 ShowNumberOfMembersInTheSet(numberOfMemeberInSet);
 ShowSetMembersKey01();
 
+//SREM
 bool isDeleted = db.SetRemove(key01, "VALUE");
 Console.WriteLine(isDeleted);
 
@@ -24,11 +28,13 @@ ShowSetMembersKey01();
 var setLength = db.SetLength(key01);
 Console.WriteLine($"Set length is: {setLength}");
 
+//SMEMEBERS
 var setMembers = db.SetMembers(key01);
 Console.WriteLine(string.Join(", ", setMembers));
 
 Console.WriteLine();
 
+//SRANDMEMBER
 for (int i = 0; i < 3; i++)
 {
     setMembers = db.SetRandomMembers(key01, 2);
@@ -45,6 +51,7 @@ for (int i = 0; i < 3; i++)
 
 Console.WriteLine();
 
+//SPOP
 for (int i = 0; i < 3; i++)
 {
     var setMemeber = db.SetPop(key01);
@@ -54,6 +61,7 @@ ShowSetMembersKey01();
 
 Console.WriteLine();
 
+//SMOVE
 db.SetMove(key01, key02, "value12");
 Console.WriteLine("Key01 set members: ");
 ShowSetMembersKey01();
@@ -63,10 +71,40 @@ ShowSetMembersKey02();
 
 Console.WriteLine();
 
-db.
+//SDIFF
+db.SetAdd(key03, ["v1", "v2", "v3"]);
+db.SetAdd(key04, ["v1", "v4", "v5"]);
+var setDiffMems1 = db.SetCombine(SetOperation.Difference, [key03, key04]);
+Console.WriteLine("SETDIFF:");
+Console.WriteLine(string.Join(", ", setDiffMems1));
 
+//SDIFFSTORE
+Console.WriteLine("SETDIFFSTORE:");
+long setDiffCount2 = db.SetCombineAndStore(SetOperation.Difference, "set:key:05", [key03, key04]);
+var setDiffMems2 = db.SetMembers("set:key:05");
+Console.WriteLine(string.Join(", ", setDiffMems2));
 
-Console.ReadLine();
+//SINETER
+Console.WriteLine("SINTER:");
+var setInterMems1 = db.SetCombine(SetOperation.Intersect, [key03, key04]);
+Console.WriteLine(string.Join(", ", setInterMems1));
+
+//SINTERSTORE
+Console.WriteLine("SINTERSTORE:");
+long setInterCount2 = db.SetCombineAndStore(SetOperation.Intersect, "set:key:06", [key03, key04]);
+var setInterMems2 = db.SetMembers("set:key:06");
+Console.WriteLine(string.Join(", ", setInterMems2));
+
+//SUNION
+Console.WriteLine("SUNION:");
+var setUnionMems1 = db.SetCombine(SetOperation.Union, [key03, key04]);
+Console.WriteLine(string.Join(", ", setUnionMems1));
+
+//SUNIONSTORE
+Console.WriteLine("SUNIONSTORE:");
+long setUnionCount2 = db.SetCombineAndStore(SetOperation.Union, "set:key:07", [key03, key04]);
+var setUnionMems2 = db.SetMembers("set:key:07");
+Console.WriteLine(string.Join(", ", setUnionMems2));
 
 void ShowSetMembersKey01()=>ShowSetMembers(key01);
 void ShowSetMembersKey02() => ShowSetMembers(key02);
---
+++ 010.Redis.DataStructure.HASH.Command/Program.cs
@@ -0,0 +1,48 @@
+﻿using StackExchange.Redis;
+
+ConnectionMultiplexer redis = ConnectionMultiplexer.Connect("localhost:6379");
+var db = redis.GetDatabase();
+const string key01 = "hash:01";
+db.KeyDelete(key01);
+
+//HMGET, HMSET, HDEL, HLEN, HEXISTS, HEKYS, HVALS, HGETALL, HINCRBY, HINCRBYFLOAT
+
+//HMSET
+List<HashEntry> hashEntries = new List<HashEntry>();
+for (int i = 0; i <= 6; i++)
+{
+    if (i < 3)
+        hashEntries.Add(new HashEntry($"key{i:d2}", $"name{i:d2}"));
+    else
+        hashEntries.Add(new HashEntry($"key{i:d2}", i));
+}
+
+db.HashSet(key01, hashEntries.ToArray());
+
+//HMGET
+Console.WriteLine(db.HashGet(key01, "key02"));
+
+
+//HDEL
+db.HashDelete(key01, "key02");
+var result = db.HashGet(key01, "key02");
+Console.WriteLine(result.HasValue ? result : "null");
+
+//HLEN
+Console.WriteLine($"Count of key-name paris in the hash: {db.HashLength(key01)}");
+
+//HEXISTS
+Console.WriteLine($"Is key03 exist: {db.HashExists(key01, "key03")}");
+Console.WriteLine($"Is key02 exist: {db.HashExists(key01, "key02")}");
+
+//HVALS
+var vals = db.HashValues(key01);
+Console.WriteLine(string.Join(", ", vals));
+
+//HGETALL
+var valsAll = db.HashGetAll(key01);
+Console.WriteLine(string.Join(", ", valsAll));
+
+//HINCRBY
+
+Console.ReadLine();
