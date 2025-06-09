4f5370c44fae6e723816786b03b98348dd5a1ab7
Kazem <kazemjavadi@hotmail.com>
2025-06-09 12:02:37 +0330

    Redis pubish subscribe

---
+++ 012.Redis.Publish.Subscribe/Program.cs
@@ -0,0 +1,27 @@
+﻿
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        //Publisher
+        Task publisherTask = Task.Run(() =>
+        {
+
+        });
+
+
+        //Subscriber
+        Task subscriberTask = Task.Run(() =>
+        {
+
+        });
+
+        Task.WaitAll(publisherTask, subscriberTask);
+    }
+}
+
+public class MessageEventArgs : EventArgs
+{
+    public string Message { get; set; }
+}
